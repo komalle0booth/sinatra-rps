3a6b6b6a7fafbd3a86672590cd9334bf6ebadfc4
komalle0booth <komalle0@chicagobooth.edu>
2024-07-05 00:05:09 +0000

    RPS Rock #1

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  @we_played = "Rock"
+  
+  @op_played = ["Rock", "Paper", "Scissors"].sample
+    
+  if @we_played == @op_played
+    @result = "We tied!"
+  elsif @we_played == "Rock" && @op_played == "Scissors"
+    @result = "We won!"
+  elsif @we_played == "Rock" && @op_played == "Paper"
+    @result = "We lost!"
+  end 
+
+  erb(:rock)
+end
